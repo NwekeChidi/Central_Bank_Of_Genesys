a17086457d20960767b9e43c3e726555264fa8b0
Chidimma Dominic Nweke <realchidinweke@gmail.com>
2021-12-14 10:11:17 +0100

    Implement User Function To Withdraw Money

--- controllers/user.js
+++ controllers/user.js
@@ -1,28 +1,35 @@
 // Import Dependencies
 const { User } = require("./../models/user");
-const { Deposit,
-    Withdrawal,
-    CreditCard,
-    DebitCard,
+const { 
+    BaseTransactions,
+    Card,
     Loan,
     Transfer } = require("./../models/userAux");
 
 
 // Deposit Money
-exports.deposit =  async (req, res) => {
+exports.base_transactions =  async (req, res) => {
     const data = req.body;
 
     const user = await User.findOne({ _id: req.USER_ID });
     if (!user) return res.status(400).send({ message: "User Not Found" });
 
+    if (data.transaction_type === "withdrawal"){
+        if (user.account_balance < data.amount) return res.status(200).send({ message : "Insufficient Funds!" })
+    }
     try {
-        const newDeposit = await new Deposit({
+        const base_transaction = await new BaseTransactions({
             user : req.USER_ID,
-            amount : data.amount
+            amount : data.amount,
+            transaction_type : data.transaction_type
         }).save();
 
-        user.account_balance += newDeposit.amount;
-        user.transactions.push(newDeposit);
+        if (data.transaction_type === "deposit"){
+            user.account_balance += base_transaction.amount
+        } else {
+            user.account_balance -= base_transaction.amount;
+        }
+        user.transactions.push(base_transaction);
 
         const updatedUser = await User.findOneAndUpdate(
             { _id : req.USER_ID },
@@ -30,11 +37,12 @@ exports.deposit =  async (req, res) => {
 
         if (!updatedUser) return res.status(400).send({ message : "Could Not Update Account"});
         
-        newDeposit.account_balance = user.account_balance;
-        newDeposit.account_number = user.account_number.substring(0, 3)+"xxxx"+user.account_number.substring(7, 10);
-        res.status(200).send({ message : "Deposit Successful!", data : newDeposit })
+        base_transaction.account_balance = user.account_balance;
+        base_transaction.account_number = user.account_number.substring(0, 3)+"xxxx"+user.account_number.substring(7, 10);
+        res.status(200).send({ message : "Transaction Successful!", data : base_transaction })
     } catch (error) {
-        res.status(400).send({ message:"Could Not Make Deposit", err:error })
+        if (error.errors.transaction_type) return res.status(400).send({ message : "Invalid Value For Type Of Transaction!"})
+        res.status(400).send({ message:"Transaction Unsuccessful!", err:error })
     }
 }
 
